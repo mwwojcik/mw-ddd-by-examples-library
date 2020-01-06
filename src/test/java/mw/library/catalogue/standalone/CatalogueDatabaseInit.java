d76233b9d140c09866dc6717edcb2190bbf51afb
mwwojcik <mw.wojcik@gmail.com>
2020-01-06 10:02:06 +0100

    small ref, adding dependencies to mapstruct, vavr (functional programming support)

---
+++ src/test/java/mw/library/catalogue/standalone/CatalogueDatabaseInit.java
@@ -0,0 +1,24 @@
+package mw.library.catalogue.standalone;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+class CatalogueDatabaseInit implements ApplicationRunner {
+    @Autowired
+    private MongoDbFactory factory;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        MongoOperations mongoOps = new MongoTemplate(factory);
+        mongoOps.dropCollection("book");
+        mongoOps.createCollection("book");
+    }
+
+
+}
