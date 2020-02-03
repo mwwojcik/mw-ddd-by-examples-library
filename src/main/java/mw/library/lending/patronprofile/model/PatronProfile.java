cd477596a16376d80997580210f63dc5bf341ee2
mwwojcik <mw.wojcik@gmail.com>
2020-02-03 18:15:51 +0100

    profiles

--- src/main/java/mw/library/lending/patronprofile/model/PatronProfile.java
+++ src/main/java/mw/library/lending/patronprofile/model/PatronProfile.java
@@ -3,24 +3,25 @@ package mw.library.lending.patronprofile.model;
 import lombok.Value;
 import mw.library.catalogue.BookId;
 
+import java.util.List;
 import java.util.Optional;
 
 @Value
 public class PatronProfile {
-    HoldsView holdsView;
-    CheckoutsView currentCheckout;
+    List<Hold> currentHolds;
+    List<Checkout> currentCheckouts;
 
     public Optional<Hold> findHold(BookId bookId) {
-        return holdsView.getCurrentHolds()
+        return getCurrentHolds()
                 .stream()
-                .filter(hold-> hold.getBook().equals(bookId))
+                .filter(hold -> hold.getBook().equals(bookId))
                 .findFirst();
-      }
+    }
 
-      public Optional<Checkout> findCheckout(BookId bookId) {
-        return currentCheckout.getCurrentCheckouts()
+    public Optional<Checkout> findCheckout(BookId bookId) {
+        return getCurrentCheckouts()
                 .stream()
-                .filter(checkout->checkout.getBook().equals(bookId))
+                .filter(checkout -> checkout.getBook().equals(bookId))
                 .findFirst();
     }
 }
