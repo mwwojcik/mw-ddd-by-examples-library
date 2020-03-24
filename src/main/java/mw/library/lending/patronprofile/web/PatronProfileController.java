541cba5a428bb7d8b89304123b876f355fd32a87
mwwojcik <mw.wojcik@gmail.com>
2020-03-24 21:47:07 +0100

    first controller methods

--- src/main/java/mw/library/lending/patronprofile/web/PatronProfileController.java
+++ src/main/java/mw/library/lending/patronprofile/web/PatronProfileController.java
@@ -7,19 +7,31 @@ import mw.library.catalogue.BookId;
 import mw.library.lending.librarybranch.model.LibraryBranchId;
 import mw.library.lending.patron.application.hold.PlaceOnHoldCommand;
 import mw.library.lending.patron.application.hold.PlacingOnHold;
+import mw.library.lending.patron.model.Hold;
 import mw.library.lending.patron.model.PatronId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
 import java.util.UUID;
 
 @RestController
 class PatronProfileController {
+
   private PlacingOnHold placingOnHold;
+
+  @GetMapping("/profiles/{patronId}/holds/{bookId}")
+  private ResponseEntity<Hold> findHold(
+      @PathVariable(name = "patronId") UUID patronId, @PathVariable(name = "bookId") UUID bookId) {
+    return null;
+  }
+
+  @GetMapping("/profiles/{patronId}/holds")
+  public ResponseEntity<Collection<Hold>> findHolds(
+      @PathVariable(name = "patronId") UUID patronId) {
+    return null;
+  }
 
   @PostMapping("/profiles/{patronId}/holds")
   public ResponseEntity placeHold(
@@ -35,6 +47,13 @@ class PatronProfileController {
     return results
         .map(success -> ResponseEntity.ok().build())
         .getOrElse(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build());
+  }
+
+  @DeleteMapping("/profiles/{patronId}/holds/{bookId}")
+  private ResponseEntity<Hold> cancelHold(
+      @PathVariable(name = "patronId") UUID patronId, @PathVariable(name = "bookId") UUID bookId) {
+
+    return null;
   }
 }
 
